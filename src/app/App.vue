6fc1856debadb0a7a6970210e90c93a8390b9757
Andrew Bubnov <79826346992@ya.ru>
2025-11-01 13:32:01 +0000

    feat: base layouting

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-
+import Input from "../shared/components/input/Input.vue";
 </script>
 
 <template>
-  
+    <Input />
 </template>
 
 <style scoped>
---
+++ src/shared/components/input/Input.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const open = ref(false)
+
+
+</script>
+
+<template>
+    <div class="container">
+        <input class="input" @focusin="open = true" @focusout="open = false" />
+        <Teleport to="body">
+            <div v-if="open" class="modal">
+                <p>Привет из модального окна!</p>
+                <button @click="open = false">Закрыть</button>
+            </div>
+        </Teleport>
+    </div>
+</template>
+
+<style scoped>
+.input {
+    anchor-name: --input;
+}
+
+.modal {
+    position: absolute;
+    position-anchor: --input;
+    top: anchor(bottom);
+    justify-self: anchor-center;
+}
+</style>
