a8cc1d56ac59343814f79a015a1f399003ab0d59
Andrew Bubnov <79826346992@ya.ru>
2025-11-03 13:16:55 +0000

    feat: icon-button component

--- src/shared/ui/components/atoms/icon-button/view.vue
+++ src/shared/ui/components/atoms/icon-button/view.vue
@@ -1,5 +1,37 @@
 <script setup lang="ts"></script>
 
-<template></template>
+<template>
+    <button class="icon-button">
+        <slot>
+            <i>✕</i>
+        </slot>
+    </button>
+</template>
 
-<style scoped></style>
+<style scoped>
+.icon-button {
+    border-radius: calc(1rem / 8);
+    padding: calc(1rem / 8);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+
+    &:focus,
+    &:focus-visible,
+    &:hover {
+        background-color: var(--accent-color);
+    }
+
+    &:active {
+        background-color: hsl(from var(--accent-color) h s calc(l - 10));
+    }
+
+    >i {
+        width: 1rem;
+        height: 1rem;
+        line-height: 1rem;
+    }
+}
+</style>
